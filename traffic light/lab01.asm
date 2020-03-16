d330e4ea47440c6d98a5e04f0b1234d912116cd1
unknown <ibrahimsamar858@gmail.com>
2020-03-16 15:31:07 +0200

    modified

--- traffic light/lab01.asm
+++ traffic light/lab01.asm
@@ -3,143 +3,9 @@
         ORG 30H
 MAIN:   MOV P1,#00H
         MOV DPTR,#200H
-A1:     MOV P1,#24H
-        MOV P0,#11H
-        MOV P2,#0A4H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
-         MOV P2,#0F9H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
-         MOV P2,#40H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
 
-
-B1:     MOV P1,#14H
-        MOV P0,#0CH
-        MOV P2,#00H
-        MOV P3,#00H
-       MOV P2,#0A4H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
-         MOV P2,#0F9H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
-         MOV P2,#40H
-        
-         MOV P3,#90H
-        ACALL DELAY
-         MOV P3,#80H
-        ACALL DELAY
-         MOV P3,#0F8H
-        ACALL DELAY
-         MOV P3,#82H
-        ACALL DELAY
-         MOV P3,#92H
-        ACALL DELAY
-         MOV P3,#99H
-        ACALL DELAY
-         MOV P3,#0B0H
-        ACALL DELAY
-         MOV P3,#0A4H
-        ACALL DELAY
-         MOV P3,#0F9H
-        ACALL DELAY
-          MOV P3,#40H
-
-C1:     MOV P1,#0AH
-        MOV P0,#24H
+A1:     MOV P1,#0AH
+        //MOV P0,#24H
         MOV P2,#0A4H
         
          MOV P3,#90H
@@ -204,8 +70,9 @@ C1:     MOV P1,#0AH
         ACALL DELAY
           MOV P3,#40H
 
-D1:     MOV P1,#21H
-        MOV P0,#22H
+
+ B1:    MOV P1,#21H
+        //MOV P0,#22H
         MOV P2,#0A4H
         
          MOV P3,#90H
@@ -269,6 +136,7 @@ D1:     MOV P1,#21H
          MOV P3,#0F9H
         ACALL DELAY
           MOV P3,#40H
+
          LJMP MAIN
 DELAY:  MOV R4,#50
 H3:     MOV R5,#20H
